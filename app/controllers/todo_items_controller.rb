76801d688624b5624539df2bff56be328c28c1d4
Sharon Gozcu <sharongozcu@gmail.com>
2016-09-23 13:11:04 +0200

    remove old URI comments to have all calls on TodoItem scoped below a TodoList.

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,11 +1,12 @@
 class TodoItemsController < ApplicationController
+  before_action :set_todo_list
   before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
 
   # GET /todo_items
   # GET /todo_items.json
-  def index
-    @todo_items = TodoItem.all
-  end
+  #def index
+  #  @todo_items = TodoItem.all
+  #end
 
   # GET /todo_items/1
   # GET /todo_items/1.json
@@ -14,7 +15,8 @@ class TodoItemsController < ApplicationController
 
   # GET /todo_items/new
   def new
-    @todo_item = TodoItem.new
+    @todo_item = @todo_list.todo_items.new
+    #@todo_item = TodoItem.new
   end
 
   # GET /todo_items/1/edit
@@ -24,11 +26,13 @@ class TodoItemsController < ApplicationController
   # POST /todo_items
   # POST /todo_items.json
   def create
-    @todo_item = TodoItem.new(todo_item_params)
+    @todo_item = @todo_list.todo_items.new(todo_item_params)
+    #@todo_item = TodoItem.new(todo_item_params)
 
     respond_to do |format|
       if @todo_item.save
-        format.html { redirect_to @todo_item, notice: 'Todo item was successfully created.' }
+        format.html { redirect_to @todo_list, notice: 'Todo item was successfully created.' }
+        #format.html { redirect_to @todo_item, notice: 'Todo item was successfully created.' }
         format.json { render :show, status: :created, location: @todo_item }
       else
         format.html { render :new }
@@ -42,7 +46,8 @@ class TodoItemsController < ApplicationController
   def update
     respond_to do |format|
       if @todo_item.update(todo_item_params)
-        format.html { redirect_to @todo_item, notice: 'Todo item was successfully updated.' }
+        format.html { redirect_to @todo_list, notice: 'Todo item was successfully updated.' }
+        #format.html { redirect_to @todo_item, notice: 'Todo item was successfully updated.' }
         format.json { render :show, status: :ok, location: @todo_item }
       else
         format.html { render :edit }
@@ -56,15 +61,22 @@ class TodoItemsController < ApplicationController
   def destroy
     @todo_item.destroy
     respond_to do |format|
-      format.html { redirect_to todo_items_url, notice: 'Todo item was successfully destroyed.' }
+      format.html { redirect_to @todo_list, notice: 'Todo item was successfully destroyed.' }
+      #format.html { redirect_to todo_items_url, notice: 'Todo item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
+
+    def set_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])
+    end
+
     def set_todo_item
-      @todo_item = TodoItem.find(params[:id])
+      @todo_item = @todo_list.todo_items.find(params[:id])
+      #@todo_item = TodoItem.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
